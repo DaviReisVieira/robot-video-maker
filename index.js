59b0e0f00016c0a9d3b5e72335376fcd41dc18ce
DaviReisVieira <vieira08davi38@gmail.com>
2020-07-26 14:00:22 -0300

    Added Text Format Robot

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const readLine = require("readline-sync");
+const robots = {
+  text: require("./robots/text"),
+};
 
-function start() {
+async function start() {
   const content = {};
 
   content.searchTerm = askAndReturnSearchTerm();
   content.prefix = askAndReturnPrefix();
+
+  await robots.text(content);
 
   function askAndReturnSearchTerm() {
     return readLine.question("Type a Wikipedia search term:");
