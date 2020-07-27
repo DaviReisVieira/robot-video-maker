7fa5ba83bd8bc1452ea20d8bd01398c3d2abdaaa
DaviReisVieira <vieira08davi38@gmail.com>
2020-07-27 19:48:05 -0300

    After Effects Done

--- robots/image.js
+++ robots/image.js
@@ -2,20 +2,16 @@ const { google } = require("googleapis");
 const customSearch = google.customsearch("v1");
 const state = require("./state");
 const imageDownloader = require("image-downloader");
-const gm = require("gm").subClass({ imageMagick: true });
 
 const googleSearchCredentials = require("../credentials/google-search.json");
 
 async function robot() {
   const content = state.load();
 
-  //await fetchImagesOfAllSentences(content);
-  //await downloadAllImages(content);
-  //await convertAllImages(content);
-  //await createAllSentenceImages(content);
-  await createYouTubeThumbnail();
+  await fetchImagesOfAllSentences(content);
+  await downloadAllImages(content);
 
-  //state.save(content);
+  state.save(content);
 
   async function fetchImagesOfAllSentences(content) {
     for (const sentence of content.sentences) {
@@ -78,140 +74,6 @@ async function robot() {
       url,
       url,
       dest: `./content/${fileName}`,
-    });
-  }
-
-  async function convertAllImages(content) {
-    for (
-      let sentenceIndex = 0;
-      sentenceIndex < content.sentences.length;
-      sentenceIndex++
-    ) {
-      await convertImage(sentenceIndex);
-    }
-  }
-
-  async function convertImage(sentenceIndex) {
-    return new Promise((resolve, reject) => {
-      const inputFile = `./content/${sentenceIndex}-original.png[0]`;
-      const outputFile = `./content/${sentenceIndex}-converted.png`;
-      const width = 1920;
-      const height = 1080;
-
-      console.log(inputFile);
-
-      gm()
-        .command("magick")
-        .in(inputFile)
-        .out("(")
-        .out("-clone")
-        .out("0")
-        .out("-background", "white")
-        .out("-blur", "0x9")
-        .out("-resize", `${width}x${height}^`)
-        .out(")")
-        .out("(")
-        .out("-clone")
-        .out("0")
-        .out("-background", "white")
-        .out("-resize", `${width}x${height}`)
-        .out(")")
-        .out("-delete", "0")
-        .out("-gravity", "center")
-        .out("-compose", "over")
-        .out("-composite")
-        .out("-extent", `${width}x${height}`)
-        .write(outputFile, (error) => {
-          if (error) {
-            return reject(error);
-          }
-
-          console.log(`> [Image-robot] Image converted: ${outputFile}`);
-          resolve();
-        });
-    });
-  }
-
-  async function createAllSentenceImages(content) {
-    for (
-      let sentenceIndex = 0;
-      sentenceIndex < content.sentences.length;
-      sentenceIndex++
-    ) {
-      await createSentenceImage(
-        sentenceIndex,
-        content.sentences[sentenceIndex].text
-      );
-    }
-  }
-
-  async function createSentenceImage(sentenceIndex, sentenceText) {
-    return new Promise((resolve, reject) => {
-      const outputFile = `./content/${sentenceIndex}-sentence.png`;
-
-      const templateSettings = {
-        0: {
-          size: "1920x400",
-          gravity: "center",
-        },
-        1: {
-          size: "1920x1080",
-          gravity: "center",
-        },
-        2: {
-          size: "800x1080",
-          gravity: "west",
-        },
-        3: {
-          size: "1920x400",
-          gravity: "center",
-        },
-        4: {
-          size: "1920x1080",
-          gravity: "center",
-        },
-        5: {
-          size: "800x1080",
-          gravity: "west",
-        },
-        6: {
-          size: "1920x400",
-          gravity: "center",
-        },
-      };
-
-      gm()
-        .command("magick")
-        .out("-size", templateSettings[sentenceIndex].size)
-        .out("-gravity", templateSettings[sentenceIndex].gravity)
-        .out("-background", "transparent")
-        .out("-fill", "white")
-        .out("-kerning", "-1")
-        .out(`caption:${sentenceText}`)
-        .write(outputFile, (error) => {
-          if (error) {
-            return reject(error);
-          }
-
-          console.log(`> [Image-robot] Sentence created: ${outputFile}`);
-          resolve();
-        });
-    });
-  }
-
-  async function createYouTubeThumbnail() {
-    return new Promise((resolve, reject) => {
-      gm()
-        .command("magick")
-        .in("./content/0-converted.png")
-        .write("./content/youtube-thumbnail.jpg", (error) => {
-          if (error) {
-            return reject(error);
-          }
-
-          console.log("> Creating YouTube Thumbnail");
-          resolve();
-        });
     });
   }
 }
